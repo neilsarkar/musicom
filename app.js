657fef7688b23b37fd0c7eee87ea21e78b8c2402
Neil Sarkar <neil@gotinder.com>
2016-04-24 14:18:14 -0700

    Add xhr handling

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var port = process.env.NODE_APP_PORT || 3000;
 
 app.use(express.static('public'));
 
+app.post('/cool', function(req,res) {
+  res.json({
+    good: 'great'
+  });
+});
+
 app.listen(port, function(err) {
   if( err ) { throw err; }
   console.log("Listening on port", port, "...");
--- app/app.js
+++ app/app.js
@@ -15,6 +15,32 @@ form.addEventListener('submit', function(e) {
     links: form.links.value,
   }
 
-  console.log("Got team", team);
+  request('POST', '/cool', { team: team }, function yes(body) {
+    console.log("Got response", body);
+  }, function no(status, body, err) {
+    console.error("Request failed", status, body, err);
+  });
+
   return false;
 });
+
+
+function request(method, url, body, success, failure) {
+  var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+  xmlhttp.open(method.toUpperCase(), url);
+  xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  body = body && JSON.stringify(body);
+  xmlhttp.onreadystatechange = function() {
+    if( xmlhttp.readyState !== XMLHttpRequest.DONE ) { return; }
+
+    if( xmlhttp.status != 200 ) {
+      return failure && failure(xmlhttp.status, xmlhttp.responseText, xmlhttp);
+    }
+
+    var response = JSON.parse(xmlhttp.responseText);
+    return success && success(response);
+  }
+
+  xmlhttp.send(body);
+}
