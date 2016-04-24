2ea2cab3597f46645dac0055e4b34236164fd10a
Neil Sarkar <neil@gotinder.com>
2016-04-24 15:47:53 -0700

    Fix up deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 host="musicalcomedyfest.com"
 folder="musicom"
 
+# Create folder if it doesn't exist
 ssh "$host" "mkdir -p /opt/src/$folder"
 
+# Minify assets
 webpack --optimize-minimize
 
+# Sync source code
 rsync \
   --exclude '.git' \
   --exclude 'node_modules' \
@@ -12,10 +15,13 @@ rsync \
   -avz \
   "./" "$host":"/opt/src/$folder"
 
+# Npm install
 ssh "$host" "cd /opt/src/$folder && NODE_ENV=production npm install"
 
-ssh "$host" "killall node"
+# Set up SSH tunnels
+ps aux | grep "ssh -Nf" | awk {'print $2'} | xargs kill -9 > /dev/null 2>&1
 ssh -NfL 5001:127.0.0.1:27017 "$host"
-ssh "$host" "cd /opt/src/$folder && NODE_ENV=production node app.js"
 
-# wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
+# Restart process
+ssh "$host" "killall node > /dev/null 2>&1"
+ssh "$host" "source ~/.bashrc && cd /opt/src/$folder && NODE_ENV=production node app.js"
