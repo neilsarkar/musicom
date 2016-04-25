04f73046820b7e8c7e3fdbd8a6464538df8c871d
Neil Sarkar <neil@gotinder.com>
2016-04-24 19:39:53 -0700

    Allow submitting form without payment

--- app/app.js
+++ app/app.js
@@ -23,24 +23,30 @@ form.addEventListener('submit', function(e) {
 
   var team = {
     name: form.name.value,
+    email: form.email.value,
     members: form.members.value,
-    coaches: form.coaches.value,
-    schools: form.schools.value,
-    description: form.description.value,
-    links: form.links.value,
+    description: form.description.value
   }
 
-  stripe.getCardToken(form.card_number.value, form.expiration_month.value, form.expiration_year.value, form.cvc.value, function(err, stripeResponse) {
-    if( err ) { console.error("Error"); return alert("Sorry, something went wrong."); }
+  if( form.card_number.value || form.expiration_month.value || form.expiration_year.value || form.cvc.value ) {
+    stripe.getCardToken(form.card_number.value, form.expiration_month.value, form.expiration_year.value, form.cvc.value, function(err, stripeResponse) {
+      if( err ) { console.error("Error"); return alert("Sorry, something went wrong."); }
 
-    team.stripe_token = stripeResponse.id;
+      team.stripe_token = stripeResponse.id;
 
+      request('POST', '/registrations', { team: team }, function yes(body) {
+        console.log("Got response", body);
+      }, function no(status, body, err) {
+        console.error("Request failed", status, body, err);
+      });
+    });
+  } else {
     request('POST', '/registrations', { team: team }, function yes(body) {
       console.log("Got response", body);
     }, function no(status, body, err) {
       console.error("Request failed", status, body, err);
     });
-  });
+  }
 
   return false;
 });
